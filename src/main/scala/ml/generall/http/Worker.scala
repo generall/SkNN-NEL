b50a58caa6fa535a4cacd201f71121f94388413a
Andrey Vasnetsov <vasnetsov93@gmail.com>
2017-04-07 22:37:44 +0300

    add timeout for worker

--- src/main/scala/ml/generall/http/Worker.scala
+++ src/main/scala/ml/generall/http/Worker.scala
@@ -3,12 +3,16 @@ package ml.generall.http
 import ml.generall.resolver.SentenceAnalizer
 import ml.generall.resolver.dto.ConceptsAnnotation
 
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+
 /**
   * Created by generall on 24.09.16.
   */
 object Worker {
   val analyzer = new SentenceAnalizer
 
-  def analyse(sentence: String): List[ConceptsAnnotation] = analyzer.analyse(sentence)
+  def analyse(sentence: String): List[ConceptsAnnotation] = Future { analyzer.analyse(sentence) }.result(2.minutes)
 
 }
