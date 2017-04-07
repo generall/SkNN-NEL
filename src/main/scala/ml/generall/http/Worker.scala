d523304e81cbbd75875b3649fda9aebc92a2eb2d
Andrey Vasnetsov <vasnetsov93@gmail.com>
2017-04-07 22:42:13 +0300

    fix await

--- src/main/scala/ml/generall/http/Worker.scala
+++ src/main/scala/ml/generall/http/Worker.scala
@@ -3,9 +3,9 @@ package ml.generall.http
 import ml.generall.resolver.SentenceAnalizer
 import ml.generall.resolver.dto.ConceptsAnnotation
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
-
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Created by generall on 24.09.16.
@@ -13,6 +13,11 @@ import scala.concurrent.duration._
 object Worker {
   val analyzer = new SentenceAnalizer
 
-  def analyse(sentence: String): List[ConceptsAnnotation] = Future { analyzer.analyse(sentence) }.result(2.minutes)
+  def analyse(sentence: String): List[ConceptsAnnotation] = {
+    val f = Future({
+      analyzer.analyse(sentence)
+    })
+    Await.result(f, 2.minutes)
+  }
 
 }
